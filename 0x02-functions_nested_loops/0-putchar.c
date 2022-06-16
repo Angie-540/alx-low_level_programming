6c819153bde268a7cd49d4a931b1f0477d6d17d9
Jay-540 <Jay-540>
2022-06-16 10:49:00 -0700

    add

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,24 @@
-gcc -Wall _putchar.c -o putchar
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: prints _putchar with _putchar
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	char c;
+	char s[]= "_putchar\n";
+
+	while (i <= 9)
+	{
+		c = s[i];
+			_putchar(c);
+			i++;
+	}
+	return (0);
+}
