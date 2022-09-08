d3b789097c5f641f6eb3b8595c8de7fcd2b1cd80
Jay-540 <96350406+Jay-540@users.noreply.github.com>
2022-09-08 22:16:02 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,12 @@
  */
 int main(void)
 {
+	char c;
+	int i;
+	long li;
+	long long ll;
+	float f;
+	
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
